f6669b74d464331494c85c6dd9b938363a3191c4
Taichi OKABE <taichi.okabe@gmail.com>
2014-07-30 13:39:37 +0900

    Revert "If be used on 64bit, re-run with WOW64."
    This reverts commit a7a587a649010c6a7896006989a6266c5bf56de8.
    Fork元の変更を取り込んだ結果、上記対応は不要となった。

--- build.bat
+++ build.bat
@@ -4,8 +4,4 @@ cd %~dp0
 echo The vbac supports Word, Excel and Access Files. Which file do you want? 
 set /p ext=File Extension (default xlsm): 
 if "%ext%" neq "" (ren "src\Ariawase.xlsm" "Ariawase.%ext%")
-if "%PROCESSOR_ARCHITECTURE%" neq "x86" (
-    %windir%\SysWOW64\cscript //nologo vbac.wsf combine
-) else {
-    cscript //nologo vbac.wsf combine
-}
+cscript //nologo vbac.wsf combine
